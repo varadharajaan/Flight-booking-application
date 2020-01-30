dfae82fd53f9447ab5bb0f46b30545318c23f89a
Varadharajan Damotharan <varadharajan.damotharan@publicissapient.com>
2020-01-30 17:42:04 +0530

    DEV: add more test cases and made docker build faster than earlier

--- src/test/java/com/invia/flightticketbooking/FlightBookingServicesTest.java
+++ src/test/java/com/invia/flightticketbooking/FlightBookingServicesTest.java
@@ -13,19 +13,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.beans.PropertyEditorRegistry;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 
-import java.beans.PropertyEditor;
+
+
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collection;
+
 import java.util.List;
-import java.util.Map;
+
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
@@ -50,229 +45,6 @@ public class FlightBookingServicesTest {
     @Mock
     FlightRepository flightRepository;
 
-    BindingResult br = new BindingResult() {
-        @Override
-        public Object getTarget() {
-            return null;
-        }
-
-        @Override
-        public Map<String, Object> getModel() {
-            return null;
-        }
-
-        @Override
-        public Object getRawFieldValue(String field) {
-            return null;
-        }
-
-        @Override
-        public PropertyEditor findEditor(String field, Class<?> valueType) {
-            return null;
-        }
-
-        @Override
-        public PropertyEditorRegistry getPropertyEditorRegistry() {
-            return null;
-        }
-
-        @Override
-        public String[] resolveMessageCodes(String errorCode) {
-            return new String[0];
-        }
-
-        @Override
-        public String[] resolveMessageCodes(String errorCode, String field) {
-            return new String[0];
-        }
-
-        @Override
-        public void addError(ObjectError error) {
-
-        }
-
-        @Override
-        public String getObjectName() {
-            return null;
-        }
-
-        @Override
-        public void setNestedPath(String nestedPath) {
-
-        }
-
-        @Override
-        public String getNestedPath() {
-            return null;
-        }
-
-        @Override
-        public void pushNestedPath(String subPath) {
-
-        }
-
-        @Override
-        public void popNestedPath() throws IllegalStateException {
-
-        }
-
-        @Override
-        public void reject(String errorCode) {
-
-        }
-
-        @Override
-        public void reject(String errorCode, String defaultMessage) {
-
-        }
-
-        @Override
-        public void reject(String errorCode, Object[] errorArgs, String defaultMessage) {
-
-        }
-
-        @Override
-        public void rejectValue(String field, String errorCode) {
-
-        }
-
-        @Override
-        public void rejectValue(String field, String errorCode, String defaultMessage) {
-
-        }
-
-        @Override
-        public void rejectValue(String field, String errorCode, Object[] errorArgs, String defaultMessage) {
-
-        }
-
-        @Override
-        public void addAllErrors(Errors errors) {
-
-        }
-
-        @Override
-        public boolean hasErrors() {
-            return false;
-        }
-
-        @Override
-        public int getErrorCount() {
-            return 0;
-        }
-
-        @Override
-        public List<ObjectError> getAllErrors() {
-            return null;
-        }
-
-        @Override
-        public boolean hasGlobalErrors() {
-            return false;
-        }
-
-        @Override
-        public int getGlobalErrorCount() {
-            return 0;
-        }
-
-        @Override
-        public List<ObjectError> getGlobalErrors() {
-            return null;
-        }
-
-        @Override
-        public ObjectError getGlobalError() {
-            return null;
-        }
-
-        @Override
-        public boolean hasFieldErrors() {
-            return false;
-        }
-
-        @Override
-        public int getFieldErrorCount() {
-            return 0;
-        }
-
-        @Override
-        public List<FieldError> getFieldErrors() {
-            return null;
-        }
-
-        @Override
-        public FieldError getFieldError() {
-            return null;
-        }
-
-        @Override
-        public boolean hasFieldErrors(String field) {
-            return false;
-        }
-
-        @Override
-        public int getFieldErrorCount(String field) {
-            return 0;
-        }
-
-        @Override
-        public List<FieldError> getFieldErrors(String field) {
-            return null;
-        }
-
-        @Override
-        public FieldError getFieldError(String field) {
-            return null;
-        }
-
-        @Override
-        public Object getFieldValue(String field) {
-            return null;
-        }
-
-        @Override
-        public Class<?> getFieldType(String field) {
-            return null;
-        }
-    };
-
-    Model model = new Model() {
-        @Override
-        public Model addAttribute(String attributeName, Object attributeValue) {
-            return null;
-        }
-
-        @Override
-        public Model addAttribute(Object attributeValue) {
-            return null;
-        }
-
-        @Override
-        public Model addAllAttributes(Collection<?> attributeValues) {
-            return null;
-        }
-
-        @Override
-        public Model addAllAttributes(Map<String, ?> attributes) {
-            return null;
-        }
-
-        @Override
-        public Model mergeAttributes(Map<String, ?> attributes) {
-            return null;
-        }
-
-        @Override
-        public boolean containsAttribute(String attributeName) {
-            return false;
-        }
-
-        @Override
-        public Map<String, Object> asMap() {
-            return null;
-        }
-    };
 
     List<Aircraft> aircraftList = new ArrayList<>();
     List<Flight> depflightList = new ArrayList<>();
@@ -421,7 +193,7 @@ public class FlightBookingServicesTest {
 
     @Test
     public  void bookingforDiffrentCraft() {
-        when(flightRepository.findAllByDepartureAirportEqualsAndDestinationAirportEqualsAndDepartureDateEquals(any(Airport.class),any(Airport.class),any(LocalDate.class))).thenReturn(depflightList);
+        //when(flightRepository.findAllByDepartureAirportEqualsAndDestinationAirportEqualsAndDepartureDateEquals(any(Airport.class),any(Airport.class),any(LocalDate.class))).thenReturn(depflightList);
 
         List<Flight> allFlightsByAirportAndDepartureTime = flightService.getAllFlightsByAirportAndDepartureTime(depAirport, desAirport, LocalDate.now());
 
@@ -429,12 +201,6 @@ public class FlightBookingServicesTest {
                 assertEquals(a.getFlightId(),1));
     }
 
-
-    @Test
-    public void flightControllerTest() {
-        controller.bookFlight(passenger, br, 23,model);
-
-    }
 
 
 }
